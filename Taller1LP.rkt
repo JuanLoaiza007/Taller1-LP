08a186ca20caee497d645c3fd3335f8536a9af92
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2023-09-08 08:25:47 -0500

    Codigo Fuente inicial

---
+++ Taller1LP.rkt
@@ -0,0 +1,23 @@
+#lang eopl
+;:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::;
+;; TALLER #1 - FUNDAMENTOS DE COMPILACION E INTERPRETACION DE LP
+
+
+;:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::;
+;; INTEGRANTES 
+; Alejandro Guerrero Cano (2179652)- Alejo101102
+; alejandro.cano@correounivalle.edu.co
+
+; Juan David Loaiza Santiago (2177570)- JuanLoaiza007
+; juan.loaiza.santiago@correounivalle.edu.co
+
+; Juan Sebastian Muñoz Rojas (2177436)- sebastianmr18
+; juan.munoz.rojas@correounivalle.edu.co
+
+
+;:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::;
+;; DESARROLLO
+
+
+
+
