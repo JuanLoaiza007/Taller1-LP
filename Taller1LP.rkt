ba4fd25eef13a8ebaf24d680d40392f91a218f4e
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2023-09-08 08:46:19 -0500

    Bocetos de funciones 2 y 5

--- Taller1LP.rkt
+++ Taller1LP.rkt
@@ -18,6 +18,33 @@
 ;:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::;
 ;; DESARROLLO
 
+#| PUNTO 2
+2) Elabore una funcion llamada down que
+recibe como argumento una lista L,
+y lo que debe realizar dicha funcion es
+retornar una lista con cada elemento de L asociado a un nivel más de
+parentesis comparado con su estado original en L.
+|#
 
+(define down
+  (lambda (l)
+    ???
+    )
+  )
 
+#| PUNTO 5
+Elabore una funcion llamada list-index que
+debe recibir dos argumentos: un predicado P y una lista L.
+La funcion retorna (desde una posicion inicial 0) el primer
+elemento de la lista que satisface el predicado L.
+
+Si llega a suceder que ningun elemento satisface el predicado recibido,
+la funcion debe retornar #f. 
+|#
+
+(define list-index
+  (lambda ()
+    ???
+    )
+  )
 
