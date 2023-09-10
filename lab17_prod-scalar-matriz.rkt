7ba66517604234ef4016f3f6725a92bf26d9738a
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2023-09-10 17:28:03 -0500

    Laboratorio de prod-scalar-matriz agregado

---
+++ lab17_prod-scalar-matriz.rkt
@@ -0,0 +1,29 @@
+#lang eopl
+
+; Espacio de juegos con Racket que NO HACE PARTE DE LA ENTREGA FINAL
+
+#| PUNTO 17
+17) Elabore una función llamada (prod-scalar-matriz mat vec)
+que recibe una matriz mat representada como una lista de listas
+y un vector vec representado como una lista y retorna el resultado
+de realizar la multiplicación matriz por vector.
+
+
+> (prod-scalar-matriz ’((1 1) (2 2)) ’(2 3))
+((2 3) (4 6))
+> (prod-scalar-matriz ’((1 1) (2 2) (3 3)) ’(2 3))
+((2 3) (4 6) (6 9))
+
+|#
+
+; Casos prueba del enunciado
+; Ni idea de como formular esto aún (en Desarrollo...)
+
+#| Teoria (en Desarrollo...)
+|#
+
+(define prod-scalar-matriz
+  (lambda (mat vec)
+    'a_ver_al_cine
+  )
+)
