3ca5c28decb684d94a16db29921312bf626dff3c
Sebastian Munoz <juan.munoz.rojas@correounivalle.edu.co>
2023-09-13 18:16:07 -0500

    Terminando funciones, a falta de pruebas.

--- Taller1LP.rkt
+++ Taller1LP.rkt
@@ -63,5 +63,118 @@ L' que intercambia cada E1 por E2, y cada E2 por E1 en L.
       ((equal? E2 (car L)) (cons E1 (swapper E1 E2 (cdr L))))
       (else (cons (car L) (swapper E1 E2 (cdr L)))))))
 
+#| 9.
+inversions
+Proposito:
+(L) --> Int: Procedimiento que recibe una lista de números L=(a1, a2, ..., an),
+esta realiza el conteo de las veces que un número ai > aj, siempre que i < j
+(es decir que haya un número mayor a otro que esta ubicado antes del segundo
+número en la lista)
+
+<lista> := ()
+<lista> := (<valor-de-scheme> <lista>)
+|#
+
+(define (inversions L)
+  (define (count-inversions L)
+    (define (count L accumulator)
+      (cond
+        ((null? L) accumulator)
+        (else
+         (let ((current (car L)))
+           (count (cdr L)
+                  (+ accumulator (count-greater current (cdr L))))))))
+    (count L 0))
+
+  (define (count-greater x L)
+    (cond
+      ((null? L) 0)
+      ((> x (car L)) (+ 1 (count-greater x (cdr L))))
+      (else (count-greater x (cdr L)))))
+
+  (count-inversions L))
+
+(inversions '(2 3 8 6 1)) 
+(inversions '(1 2 3 4))  
+(inversions '(3 2 1))     
+
+#| 12. filter-acum
+
+
+|#
+
+(define (filter-acum a b F acum filter)
+  (define (iterator i accumulator)
+    (cond
+      ((> i b) accumulator)
+      ((filter i)
+       (iterator (+ i 1) (F accumulator i)))
+      (else
+       (iterator (+ i 1) accumulator))))
+  (iterator a acum))
+
+; Ejemplos de uso
+(filter-acum 1 10 + 0 odd?)  ; Debería imprimir 25
+(filter-acum 1 10 + 0 even?) ; Debería imprimir 30
+
+#|15.
+
+
+|#
+(define (count-odd-and-even arbol)
+  (define (count-odd-and-even-aux subarbol)
+    (cond
+      ((null? subarbol) '(0 0))
+      ((pair? subarbol)
+       (let* ((nodo (car subarbol))
+              (subarbol-izq (cadr subarbol))
+              (subarbol-der (caddr subarbol))
+              (conteo-izq (count-odd-and-even-aux subarbol-izq))
+              (conteo-der (count-odd-and-even-aux subarbol-der))
+              (conteo-izq-par (car conteo-izq))
+              (conteo-izq-impar (cadr conteo-izq))
+              (conteo-der-par (car conteo-der))
+              (conteo-der-impar (cadr conteo-der)))
+         (if (even? nodo)
+             (list (+ 1 conteo-izq-par conteo-der-par) (+ conteo-izq-impar conteo-der-impar))
+             (list (+ conteo-izq-par conteo-der-par) (+ 1 conteo-izq-impar conteo-der-impar)))))
+      ((number? subarbol)
+       (if (even? subarbol)
+           '(1 0)
+           '(0 1)))
+      (else '(0 0))))
+  (count-odd-and-even-aux arbol))
+
+(define arbol-ejemplo '(14 (7 () (12 () ())) (26 (20 (17 () ()) ()) (31 () ()))))
+
+
+(count-odd-and-even arbol-ejemplo)
+
+#|18.
+
+
+|#
+
+
+(define (pascal N)
+  (define (calculate-element row col)
+    (cond
+      ((or (= col 0) (= col row)) 1)
+      (else (+ (calculate-element (- row 1) (- col 1))
+               (calculate-element (- row 1) col)))))
+  
+  (define (construct-row row)
+    (define (construct-row-helper col)
+      (if (= col row)
+          (list (calculate-element row col))
+          (cons (calculate-element row col) (construct-row-helper (+ col 1)))))
+    (construct-row-helper 0))
+  
+  (if (< N 0)
+      '()
+      (construct-row (- N 1))))
+
+
+
 
 
